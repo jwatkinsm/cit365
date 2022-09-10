2d58704be5f029f2369526cc7cee92db141a3d13
Joshua Watkins <jwatkinsm@byui.edu>
2022-09-10 18:23:12 -0400

    glass file

--- ConsoleApp/GlazerApp.cs
+++ ConsoleApp/GlazerApp.cs
@@ -1,8 +1,29 @@
 ﻿using System;
-
-public class Class1
+namespace ConsoleApp
 {
-	public Class1()
+
+	internal class GlazerApp
 	{
-	}
+		public static void RunExample()
+		{
+            //variables
+            double width, height, woodLength, glassArea;
+            string widthString, heightString;
+            //user input
+            Console.WriteLine("\nEnter the width of the window");
+            widthString = Console.ReadLine();
+            width = double.Parse(widthString);
+            Console.WriteLine("Enter the hieght of window");
+            heightString = Console.ReadLine();
+            //calculate
+            height = double.Parse(heightString);
+            woodLength = 2 * (width + height) * 3.25;
+            glassArea = 2 * (width * height);
+            //print result
+            Console.WriteLine("The length of the wood is " +
+            woodLength + " feet");
+            Console.WriteLine("The area of the glass is " +
+            glassArea + " square metres");
+        }
+    }
 }
