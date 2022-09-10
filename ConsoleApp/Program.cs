cc5a7f6fb0592c3a084b3b12b4a4b2081e2d94ca
Joshua Watkins <jwatkinsm@byui.edu>
2022-09-10 18:02:25 -0400

    user countdown

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -10,40 +10,71 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {//variables
-            string fullName = "Joshua Watkins";
-            string location = "Stafford, Virginia";
-            //print name and location
-            Console.WriteLine("My name is {0}, I am from {1}.", fullName, location);
-           
+         //string fullName = "Joshua Watkins";
+         // string location = "Stafford, Virginia";
+         //print/call name and location
+            GetUserNameAndLocation();
+            //Console.WriteLine("My name is {0}, I am from {1}.", fullName, location);
+
             //christmas countdown
             //variables
-            DateTime today = DateTime.Today;
-            DateTime christmas = new DateTime(2022, 12, 25);
-            //print date and countdown
-            Console.WriteLine("\nCurrent date "+ today.ToString("d"));
-            Console.WriteLine("{0} days until Christmas.", (christmas - today).Days);
+            // DateTime today = DateTime.Today;
+            // DateTime christmas = new DateTime(2022, 12, 25);
+            //print/call date and countdown
+            ChristmasCountdown();
+            // Console.WriteLine("\nCurrent date "+ today.ToString("d"));
+            // Console.WriteLine("{0} days until Christmas.", (christmas - today).Days);
 
             //window measurements 2.1.1
             //variables
-            double width, height, woodLength, glassArea;
-            string widthString, heightString;
+            // double width, height, woodLength, glassArea;
+            //string widthString, heightString;
             //user input
-            Console.WriteLine("\nEnter the width of the window");
-            widthString = Console.ReadLine();
-            width = double.Parse(widthString);
-            Console.WriteLine("Enter the hieght of window");
-            heightString = Console.ReadLine();
+            //Console.WriteLine("\nEnter the width of the window");
+            //widthString = Console.ReadLine();
+            // width = double.Parse(widthString);
+            // Console.WriteLine("Enter the hieght of window");
+            //heightString = Console.ReadLine();
             //calculate
-            height = double.Parse(heightString);
-            woodLength = 2 * (width + height) * 3.25;
-            glassArea = 2 * (width * height);
+            // height = double.Parse(heightString);
+            //woodLength = 2 * (width + height) * 3.25;
+            // glassArea = 2 * (width * height);
             //print result
-            Console.WriteLine("The length of the wood is " +
-            woodLength + " feet");
-            Console.WriteLine("The area of the glass is " +
-            glassArea + " square metres");
+            //Console.WriteLine("The length of the wood is " +
+            //woodLength + " feet");
+            //Console.WriteLine("The area of the glass is " +
+            // glassArea + " square metres");
+            Console.WriteLine("Press any key to exit...");
+            _ = Console.ReadKey();
+            //Console.ReadLine ();
+        }
+        private static void GetUserNameAndLocation()
+        {
+           //user input name Location
+            Console.WriteLine("What is your name? ");
+            var personName = Console.ReadLine();
+            Console.WriteLine($"Hi {personName}! Where are you from? ");
+            var personLocation = Console.ReadLine();
+            Console.WriteLine($"I have never been to {personLocation}. I bet it is nce. \nPress  any key to continue...");
+            Console.ReadKey();
 
-            Console.ReadLine();
+            //variables
+            var Person = new person
+            {
+                Name = personName,
+                Location = personLocation
+            };
+        }
+        private static void ChristmasCountdown() 
+        {
+            //variables
+            DateTime today = DateTime.Today;
+            DateTime christmas = new DateTime(2022, 12, 25);
+            var daysLeft = (christmas - today).Days;
+            //calculate
+            Console.WriteLine($"Today is {today.ToString("d")}");
+            Console.WriteLine($"There are {daysLeft} days until Christmas");
+            Console.WriteLine("Press any key to continue.");
             Console.ReadKey();
         }
     }
---
+++ ConsoleApp/person.cs
@@ -0,0 +1,8 @@
+﻿namespace ConsoleApp
+{
+    internal class person
+    {
+        public string Name { get; set; }
+        public string Location { get; set; }
+    }
+}
