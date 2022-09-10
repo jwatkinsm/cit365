d14a2fa5de2d470e31b1ed41ae16282bd951d434
Joshua Watkins <jwatkinsm@byui.edu>
2022-09-10 16:02:32 -0400

    comments

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -24,16 +24,20 @@ namespace ConsoleApp
             Console.WriteLine("{0} days until Christmas.", (christmas - today).Days);
 
             //window measurements 2.1.1
+            //variables
             double width, height, woodLength, glassArea;
             string widthString, heightString;
+            //user input
             Console.WriteLine("\nEnter the width of the window");
             widthString = Console.ReadLine();
             width = double.Parse(widthString);
             Console.WriteLine("Enter the hieght of window");
             heightString = Console.ReadLine();
+            //calculate
             height = double.Parse(heightString);
             woodLength = 2 * (width + height) * 3.25;
             glassArea = 2 * (width * height);
+            //print result
             Console.WriteLine("The length of the wood is " +
             woodLength + " feet");
             Console.WriteLine("The area of the glass is " +
