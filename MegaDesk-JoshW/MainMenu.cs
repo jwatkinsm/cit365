0689e0c176297333caaa769aa6c7554553399561
Joshua Watkins <jwatkinsm@byui.edu>
2022-09-26 17:42:30 -0400

    megadesk intial

---
+++ MegaDesk-JoshW/MainMenu.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MegaDesk_JoshW
+{
+    public partial class MainMenu : Form
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void ExitBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SearchQuotesBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ViewQuotesBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AddQuoteBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
