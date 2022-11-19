f99836f4c62a3885931ce5d40451d68c31e19e70
jwatkinsm <49845925+jwatkinsm@users.noreply.github.com>
2022-11-18 22:46:39 -0500

    Inheritence T2 but no data

---
+++ NeverUniversity/Models/SchoolViewModels/EnrollmentDateGroup.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace NeverUniversity.Models.SchoolViewModels
+{
+    public class EnrollmentDateGroup
+    {
+        [DataType(DataType.Date)]
+        public DateTime? EnrollmentDate { get; set; }
+
+        public int StudentCount { get; set; }
+    }
+}
